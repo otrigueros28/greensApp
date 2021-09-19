e7fcfa7e53b8dbaaea6e518b24e10e2ee329265a
Oscar Trigueros <otrigueros28>
2021-09-18 19:09:15 -0700

    start

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, Alert, Button, TouchableOpacity} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Button color ="white" title = "Click Me" onPress= {() =>
+      Alert.alert("Title", "it was clicked", [
+        {text: "yes"},
+        {text: "no"}]
+        )}/>
+      <Text>beginning of something new, not sure what</Text>
+      <TouchableOpacity>
+        <Image
+        source={{
+          width: 200,
+          height: 300,
+          uri: "https://picsum.photos/200/300"}} />
+      </TouchableOpacity>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightgreen',
     alignItems: 'center',
     justifyContent: 'center',
   },
