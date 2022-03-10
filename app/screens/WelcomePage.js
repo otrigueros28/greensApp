f8d7b4ec80f41b65e88ef885ea3fe099b1996deb
Oscar Trigueros <otrigueros28@yahoo.com>
2022-03-09 19:42:01 -0800

    just quick edit to welcome screen

--- app/screens/WelcomePage.js
+++ app/screens/WelcomePage.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { ImageBackground, View, Image, StyleSheet, Text} from 'react-native';
+import { borderBottomColor, borderLeftColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
 
 function WelcomePage(props) {
   return (
    <ImageBackground
    style={styles.background}
-   source={require("../../assets/back.jpg")}
+   source={require("../../assets/back2.jpg")}
   >
     <View style= {styles.logoContainer}>
-        <Image style= {styles.logo} source= {require("../../assets/logo.jpg")}></Image>
-        <Text> Trade Earths Bounty</Text>
+      <Text style= {styles.titleText}>Sublime</Text>
+       {/*<Image style= {styles.logo} source= {require("../../assets/logo.jpg")}></Image>   not sure about a logo, will need to find one that i like and make it work*/}
+        <Text style= {styles.subheader}> Trade Earths Bounty</Text>
     </View>
     <View style= {styles.loginButton}>
       <Text style= {styles.buttonText}>Login</Text>
@@ -32,13 +34,17 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 70,
     backgroundColor: 'white',
-    alignItems: 'center'
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: 'lightgray',
+    borderRadius: 20
   },
   registerButton:{
     width: '100%',
     height: 70,
     backgroundColor: 'lightgrey',
-    alignItems: 'center'
+    alignItems: 'center',
+    borderRadius: 20
   },
   logo: {
     width: 100,
@@ -46,12 +52,19 @@ const styles = StyleSheet.create({
   },
   logoContainer: {
     position: "absolute",
-    top: 90,
-    alignItems: 'center'
+    top: 330,
+    left: 0
   },
   buttonText: {
     fontSize: 25,
     top: 20
+  },
+  titleText: {
+    fontSize: 35,
+    fontWeight: 'bold',
+  },
+  subheader: {
+    fontSize:20
   }
 })
 
